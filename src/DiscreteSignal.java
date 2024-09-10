6505f4e54f326afb9b985c0e4f169f1dcb4efc59
lbphucc <lbphucccc@gmail.com>
2024-09-10 08:51:16 +0700

    sửa

--- src/DiscreteSignal.java
+++ src/DiscreteSignal.java
@@ -1,6 +1,8 @@
-// Lớp DiscreteSignal theo định nghĩa tín hiệu rời rạc
-class DiscreteSignal1 {
-    // Phương thức tính toán tín hiệu rời rạc x(n)
+public class DiscreteSignal1 {
+    // Hàm d(x) là dãy xung đơn vị
+    public int unitImpulse(int n) {
+        return (n == 0) ? 1 : 0;
+    }
     public double calculateDiscreteSignal(int n, int[] unitImpulse, int[] inputSignal) {
         double result = 0.0;
         int length = Math.min(unitImpulse.length, inputSignal.length);
@@ -12,10 +14,5 @@ class DiscreteSignal1 {
         }
 
         return result;
-    }
-
-    // Hàm d(x) là dãy xung đơn vị
-    public int unitImpulse(int n) {
-        return (n == 0) ? 1 : 0;
     }
 }
