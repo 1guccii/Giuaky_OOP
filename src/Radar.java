b2b710c6dd3fe4a611dd34db13cbe1d91b6c0d28
lbphucc <lbphucccc@gmail.com>
2024-09-10 09:07:05 +0700

    Update Radar.java

--- src/Radar.java
+++ src/Radar.java
@@ -1,10 +1,10 @@
 class Radar {
     public double[] analyzeDiscreteSignal(int n) {
-        double[] discreteSignal = new double[16];
+        double[] DiscreteSignal = new double[16];
         for (int i = 0; i < 16; i++) {
-            discreteSignal[i] = (i >= 0 && i <= 15) ? (1 - i / 15.0) : 0;
+            DiscreteSignal[i] = (i >= 0 && i <= 15) ? (1 - i / 15.0) : 0;
         }
-        return discreteSignal;
+        return DiscreteSignal;
     }
 
     public void printDiscreteSignal(double[] signal) {
